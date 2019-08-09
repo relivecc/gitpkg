c2cf2361cf2464fba7d3e57768f1508ca7cab51e
Yousef El-Dardiry <yousefdardiry@gmail.com>
2019-08-09 16:42:10 +0200

    don't add version to tag

--- src/tasks/Publish/get-git-tag-name.js
+++ src/tasks/Publish/get-git-tag-name.js
@@ -1,6 +1,6 @@
 import { normalisePackageNameNpm } from './normalise-package-name';
 
 export default function getGitTagName(pkg) {
-  const gitpkgPackageName = `${normalisePackageNameNpm(pkg.name)}-v${pkg.version}-gitpkg`;
+  const gitpkgPackageName = `${normalisePackageNameNpm(pkg.name)}`;
   return gitpkgPackageName;
 }

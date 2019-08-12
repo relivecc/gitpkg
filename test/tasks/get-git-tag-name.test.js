487c4338b29a4d1341b01d4e9cf235069f31db02
Yousef El-Dardiry <yousefdardiry@gmail.com>
2019-08-12 15:15:24 +0200

    fix unit test

--- test/tasks/get-git-tag-name.test.js
+++ test/tasks/get-git-tag-name.test.js
@@ -1,9 +1,9 @@
-import getGitTagName from '../../src/tasks/Publish/get-git-tag-name';
+import getGitTagName from "../../src/tasks/Publish/get-git-tag-name";
 
-const pkg = { name: 'megapkg', version: '1.0.0' };
-const gitTagName = `${pkg.name}-v${pkg.version}-gitpkg`;
+const pkg = { name: "megapkg", version: "1.0.0" };
+const gitTagName = `${pkg.name}`;
 
-describe('while using getGitTagName()', () => {
+describe("while using getGitTagName()", () => {
   it(`should return "${gitTagName}"`, () => {
     expect(getGitTagName(pkg)).toBe(gitTagName);
   });
